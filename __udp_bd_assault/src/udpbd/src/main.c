a3543add8382554566514b3efa22dfd640d94161
israpps <57065102+israpps@users.noreply.github.com>
2023-12-19 11:27:23 -0300

    combine UDPBD and DEV9 into a standalone module
    dev9 exports remain, since DEV9 is required more often than SMAP/UDPBD by other modules

---
+++ __udp_bd_assault/src/udpbd/src/main.c
@@ -0,0 +1,33 @@
+#include <errno.h>
+#include <stdio.h>
+#include <loadcore.h>
+#include <thbase.h>
+#include <irx.h>
+
+#include "include/main.h"
+#include "include/xfer.h"
+
+// Last SDK 3.1.0 has INET family version "2.26.0"
+// SMAP module is the same as "2.25.0"
+//IRX_ID("SMAP_driver", 0x2, 0x1A);
+
+//While the header of the export table is small, the large size of the export table (as a whole) places it in data instead of sdata.
+///extern struct irx_export_table _exp_smap __attribute__((section("data")));
+
+int udpbd_start(int argc, char *argv[])
+{
+    int result;
+
+//    if (RegisterLibraryEntries(&_exp_smap) != 0) {
+//        PRINTF("smap: module already loaded\n");
+//        return MODULE_NO_RESIDENT_END;
+//    }
+
+    if ((result = smap_init(argc, argv)) < 0) {
+        PRINTF("smap: smap_init -> %d\n", result);
+        //ReleaseLibraryEntries(&_exp_smap);
+        return MODULE_NO_RESIDENT_END;
+    }
+
+    return MODULE_RESIDENT_END;
+}
